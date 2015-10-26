95f1f204bcf1cc276e3a00cba5636311247f3009
Nikos Lykouropoulos <nikos.lykouropoulos@gmail.com>
2015-10-26 13:04:34 +0200

    setup CMIS connection to ModelShape

---
+++ workflow/workflow-engine/src/test/java/gr/cyberstream/workflow/engine/cmis/test/CMISConnect.java
@@ -0,0 +1,44 @@
+package gr.cyberstream.workflow.engine.cmis.test;
+
+import static org.junit.Assert.*;
+
+import org.apache.chemistry.opencmis.client.api.Session;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import gr.cyberstream.workflow.engine.cmis.CMISSession;
+import gr.cyberstream.workflow.engine.config.test.ApplicationConfiguration;
+
+@ContextConfiguration(classes = ApplicationConfiguration.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class CMISConnect {
+	
+	final static Logger logger = LoggerFactory.getLogger(CMISConnect.class);
+
+	@Autowired
+	private CMISSession cmisSession;
+
+	@Test
+	public void testCleanUp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSession() {
+		Session session = cmisSession.getSession();
+		
+		assertNotNull("CMIS Session should not be null", session);
+		logger.info(session.toString());
+	}
+
+	@Test
+	public void testSetSession() {
+		fail("Not yet implemented");
+	}
+
+}
